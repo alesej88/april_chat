a1f15430f91350cce46f1c60a24e4624dfd889a2
alesej88 <alesej88@ya.ru>
2021-05-05 16:03:00 +0600

    HW4

--- chat_client/src/main/java/ru/geekbrains/chat/client/MainChatController.java
+++ chat_client/src/main/java/ru/geekbrains/chat/client/MainChatController.java
@@ -9,7 +9,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import javax.swing.*;
 import java.awt.*;
+import java.awt.MenuItem;
+import java.awt.event.WindowEvent;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -31,6 +34,12 @@ public class MainChatController {
     }
 
     public void showAbout(ActionEvent actionEvent) {
+        WindowEvent WE = new WindowEvent;
+        WE.getWindow();
+
+
+
+
 
     }
 
