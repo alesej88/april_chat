3fa4ab6cd7ce15a2acecfae13ecd5fdb99268881
alesej88 <alesej88@ya.ru>
2021-05-05 11:38:30 +0600

    Home work 4

---
+++ chat_client/src/main/java/ru/geekbrains/chat/client/MainChatController.java
@@ -0,0 +1,53 @@
+package ru.geekbrains.chat.client;
+
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class MainChatController {
+
+
+    public TextArea chatArea;
+    public ListView onlineUsers;
+    public TextField inputField;
+    public Button btnSendMessage;
+
+    public void mockAction(ActionEvent actionEvent) {
+        System.out.println("MOCK!");
+    }
+
+    public void exit(ActionEvent actionEvent) {
+        Platform.exit();
+    }
+
+    public void showAbout(ActionEvent actionEvent) {
+
+    }
+
+    public void showHelp(ActionEvent actionEvent) throws URISyntaxException, IOException {
+        Desktop desktop = Desktop.getDesktop();
+        desktop.browse(new URI("https://docs.google.com/document/d/1wr0YEtIc5yZtKFu-KITqYnBtp8KC28v2FEYUANL0YAM/edit?usp=sharing"));
+    }
+
+    public void sendMessage(ActionEvent actionEvent) {
+        appendTextFromTF();
+    }
+
+    private void appendTextFromTF() {
+        String msg = inputField.getText();
+        if (msg.isEmpty()) return;
+        chatArea.appendText("ME: " + msg + System.lineSeparator());
+//        chatArea.set
+        inputField.clear();
+    }
+}
